fb94ebef172f54a8c6657b377bc851a02a8a538a
Rakshat28 <rakshat07ps@gmail.com>
2024-11-02 13:21:01 +0530

    new way to use Context API from Hitesh Chaudhary

---
+++ React/PropDrilling/Context-API2/src/components/Profile.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import UserContext from '../Contexts/UserContext'
+import { useContext } from 'react';
+function Profile() {
+    const {user} = useContext(UserContext);
+    if(!user){
+        return(
+            <div>
+                please login 
+            </div>
+        )
+    }
+    return( 
+        <div>
+            Welcome {user.username}
+        </div>
+    )
+}
+
+export default Profile
