e3150329b7042fa79a1e9286c162b64302645f43
Rakshat28 <rakshat07ps@gmail.com>
2024-11-24 03:25:38 +0530

    file structure of ts codebase

---
+++ TypeScript/learning-ts/src/test.ts
@@ -0,0 +1,52 @@
+// const x:number = 1;
+// console.log(x);
+// ..........................................................................
+
+function greet(firstName:string){
+    console.log("hello " + firstName);
+}
+
+greet("Rakshat");
+
+// ...............................................
+
+function sum(num1:number,num2:number):number{
+    return num1+num2;
+}
+sum(1,2);
+
+// ...............................................
+function isLegal(age:number):boolean{
+    return age>18 ? true:false;
+}
+
+console.log(isLegal(23));
+
+// .............................................
+
+function callback(fn:()=>number):void
+    {
+        setTimeout(fn,1000);
+    }
+callback(function (){
+    console.log("hello ji");
+    return 1;
+})
+
+// .......................................
+
+// interfaces in TS
+interface User{
+    firstName:string,
+    lastName:string,
+    age:number
+}
+
+function isAdult(user : User):boolean{
+    return user.age>18?true:false;
+}
+console.log(isAdult({
+    firstName:'Rakshat',
+    lastName:'Singh',
+    age:21
+}));
