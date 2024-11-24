7223793b10374a48f297f39aaf3f4d5cfa3e240d
Rakshat28 <rakshat07ps@gmail.com>
2024-11-24 18:44:27 +0530

    issues fixed

--- TypeScript/Generics/src/test.ts
+++ TypeScript/Generics/src/test.ts
@@ -8,9 +8,10 @@ let output1= identity<string>("myString");
 let output2 = identity<number>(100);
 
 // .....................................................
+
 function getFirstElement<T>(arr: T[]) {
     return arr[0];
 }
 
-const el = getFirstElement(["harkiratSingh", "ramanSingh"]);
+const el = getFirstElement<string>(["harkiratSingh", "2"]);
 console.log(el.toLowerCase())
